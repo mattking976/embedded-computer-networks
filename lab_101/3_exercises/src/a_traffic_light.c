cb1444f3778015e44b7d83905b8f2e0c8085733a
mattking976 <matthew_d_king@outlook.com>
2018-10-04 14:54:07 +0100

    Tasks 1 and 2 completed traffic lights remains unfinished

--- lab_101/3_exercises/src/a_traffic_light.c
+++ lab_101/3_exercises/src/a_traffic_light.c
@@ -13,11 +13,30 @@
 #include "clock.h"
 #include "gpio.h"
 
-// map the led to GPIO PA8
+// map the leds to GPIO PI1 PI14 PI15 for red amber and green lights
 gpio_pin_t led = {PI_1, GPIOI, GPIO_PIN_1};
+gpio_pin_t led2 = {PB_14, GPIOI, GPIO_PIN_14};
+gpio_pin_t led3 = {PB_15, GPIOI, GPIO_PIN_15};
 
 // this is the main method
 int main()
 {
-
+	//turning the red led on
+	write_gpio(led2, LOW);
+	write_gpio(led, HIGH);
+	//wait while on red
+	HAL_Delay(1000);
+	//transition to red amber state
+	write_gpio(led2, HIGH);
+	//wait while on red amber
+	HAL_Delay(1000);
+	//turn on green and turn off red amber
+	write_gpio(led, LOW);
+	write_gpio(led2, LOW);
+	write_gpio(led3, HIGH);
+	//wait while on green
+	HAL_Delay(1000);
+	write_gpio(led3, LOW);
+	write_gpio(led2, HIGH);
+	HAL_Delay(1000);
 }
