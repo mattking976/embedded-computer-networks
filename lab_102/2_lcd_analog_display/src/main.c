aacb5b4ed6cb064303bb14adf90bc32b5ff33407
matthew_d_king@outlook.com <matthew_d_king@outlook.com>
2018-10-18 14:52:41 +0100

    task 2 and task 1 from lab 2

--- lab_102/2_lcd_analog_display/src/main.c
+++ lab_102/2_lcd_analog_display/src/main.c
@@ -14,11 +14,17 @@
 // include the shu bsp libraries for the stm32f7 discovery board
 #include "pinmappings.h"
 #include "clock.h"
+#include "adc.h"
 #include "stm32746g_discovery_lcd.h"
 
 // LCD DEFINES
-
+#define BOARDER "****************************"
 // CODE
+const char * welcome_Message[2] = 
+{
+	"*     Hello LCD World!     *",
+	"*      Welcome to SHU      *"
+};
 
 // this is the main method
 int main()
@@ -27,5 +33,20 @@ int main()
   // properly
   HAL_Init();
   init_sysclk_216MHz();
-
+	
+	BSP_LCD_Init();
+	BSP_LCD_LayerDefaultInit(LTDC_ACTIVE_LAYER, SDRAM_DEVICE_ADDR);
+	BSP_LCD_SelectLayer(LTDC_ACTIVE_LAYER);
+	
+	BSP_LCD_SetBackColor(LCD_COLOR_BLACK);
+	BSP_LCD_Clear(LCD_COLOR_BLACK);
+	
+	BSP_LCD_SetFont(&Font24);
+	
+	BSP_LCD_SetTextColor(LCD_COLOR_WHITE);
+	BSP_LCD_DisplayStringAtLine(0, (uint8_t *)BOARDER);
+	BSP_LCD_DisplayStringAtLine(1, (uint8_t *)welcome_Message[0]);
+	BSP_LCD_DisplayStringAtLine(2, (uint8_t *)welcome_Message[1]);
+	BSP_LCD_DisplayStringAtLine(3, (uint8_t *)BOARDER);
+	HAL_Delay(5);
 }
