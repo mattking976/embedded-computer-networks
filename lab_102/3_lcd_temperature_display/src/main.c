d22a7de30cdc88921e0f7e751e6196fee27a3594
Matt <matthew_d_king@outlook.com>
2018-11-08 14:59:58 +0000

    worked on lab

--- lab_102/3_lcd_temperature_display/src/main.c
+++ lab_102/3_lcd_temperature_display/src/main.c
@@ -14,11 +14,19 @@
 // include the shu bsp libraries for the stm32f7 discovery board
 #include "pinmappings.h"
 #include "clock.h"
+#include "adc.h"
 #include "stm32746g_discovery_lcd.h"
 
 // LCD DEFINES
-
+#define BOARDER "****************************"
 // CODE
+const char * welcome_Message[2] = 
+{
+	"*     Hello LCD World!     *",
+	"*      Welcome to SHU      *"
+};
+
+gpio_pin_t pot = {PA_0, GPIOA, GPIO_PIN_0};
 
 // this is the main method
 int main()
@@ -27,5 +35,37 @@ int main()
   // properly
   HAL_Init();
   init_sysclk_216MHz();
-
+	init_adc(pot);
+	
+	BSP_LCD_Init();
+	BSP_LCD_LayerDefaultInit(LTDC_ACTIVE_LAYER, SDRAM_DEVICE_ADDR);
+	BSP_LCD_SelectLayer(LTDC_ACTIVE_LAYER);
+	
+	// show the header
+	BSP_LCD_SetBackColor(LCD_COLOR_BLACK);
+	BSP_LCD_Clear(LCD_COLOR_BLACK);
+	
+	BSP_LCD_SetFont(&Font24);
+	
+	BSP_LCD_SetTextColor(LCD_COLOR_GREEN);
+	BSP_LCD_DisplayStringAtLine(0, (uint8_t *)BOARDER);
+	BSP_LCD_DisplayStringAtLine(1, (uint8_t *)welcome_Message[0]);
+	BSP_LCD_DisplayStringAtLine(2, (uint8_t *)welcome_Message[1]);
+	BSP_LCD_DisplayStringAtLine(3, (uint8_t *)BOARDER);
+	
+	// loop
+	while(1)
+	{
+		uint16_t adc_val = read_adc(pot);
+		char str[20];
+		sprintf(str, "ADC = %4d", adc_val);
+		BSP_LCD_ClearStringLine(6);
+		BSP_LCD_DisplayStringAtLine(6, (uint8_t *)str);
+		
+		sprintf(str, "ADC percent = %4f", ((((adc_val/4095.0)*3300.0) -500 )/10.0));
+		BSP_LCD_ClearStringLine(7);
+		BSP_LCD_DisplayStringAtLine(7, (uint8_t *)str);
+		
+		HAL_Delay(75);
+	}
 }
